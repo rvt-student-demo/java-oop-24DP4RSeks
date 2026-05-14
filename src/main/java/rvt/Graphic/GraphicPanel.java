277b00f840093613e423c163f80a9987445fb562
24DP4RSeks <A240794RS@rvt.lv>
2026-05-14 10:56:42 +0300

    Fixed Grahpic

--- src/main/java/rvt/Graphic/GraphicPanel.java
+++ src/main/java/rvt/Graphic/GraphicPanel.java
@@ -1,14 +1,15 @@
 package rvt.Graphic;
 
-import javax.imageio.ImageIO;
-import javax.swing.JPanel;
-
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.LayoutManager;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
 
 public class GraphicPanel extends JPanel {
 
@@ -34,25 +35,7 @@ public class GraphicPanel extends JPanel {
     }
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        
-        if (myImage != null) {
-            g.drawImage(myImage, 0, 0, 1900, 100, this);
-            g.drawImage(myImage, 0, 100, 1900, 100, this);
-            g.drawImage(myImage, 0, 200, 1900, 100, this);
-            g.drawImage(myImage, 0, 300, 1900, 100, this);
-            g.drawImage(myImage, 0, 400, 1900, 100, this);
-            g.drawImage(myImage, 0, 500, 1900, 100, this);
-            g.drawImage(myImage, 0, 600, 1900, 100, this);
-            g.drawImage(myImage, 0, 700, 1900, 100, this);
-            g.drawImage(myImage, 0, 800, 1900, 100, this);
-            g.drawImage(myImage, 0, 900, 1900, 100, this);
-            g.drawImage(myImage, 0, 1000, 1900, 100, this);
-            g.drawImage(myImage, 0, 1100, 1900, 100, this);
-
-        } else {
-            g.setColor(Color.RED);
-            g.drawString("Image failed to load", 1900, 1000);
-        }
+       g.setColor(Color.BLACK);
+       g.setFont(Font.getFont("Arial").deriveFont(24f));
     }
 }
